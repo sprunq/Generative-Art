d09abf847a210cc855401a4112b0ec7a8a6b52ee
sprunq <tobias.laurin@web.de>
2022-07-03 22:33:00 +0200

    Desert

--- DomainWarping/sketch.js
+++ DomainWarping/sketch.js
@@ -1,18 +1,28 @@
 var runOnce = true;
 var f0, f1, f2;
-var cWhite, cBlack, cPurple, cWineRed, cWhiteGrey, cLighterWineRed, cBlue;
+var root;
 function setup() {
   createCanvas(3840, 2160);
-  cWhite = color(255);
-  cBlack = color(0);
-  cPurple = color(51, 26, 102);
-  cWineRed = color(77, 13, 13);
-  cWhiteGrey = color(220);
-  cLighterWineRed = color(128, 51, 51);
-  cBlue = color(0, 51, 102);
-}
+  root = {
+    cWhite: color(255),
+    cBlack: color(0),
+    cGrey: color(50),
+    cPurple: color(51, 26, 102),
+    cWineRed: color(77, 13, 13),
+    cWhiteGrey: color(230),
+    cClouds: color(230),
+    cLighterWineRed: color(128, 51, 51),
+    cBlue: color(0, 190, 255),
+    cGreen: color(0, 100, 0),
+    cBrown: color(181, 114, 42),
+    cLightBlue: color(97, 255, 255),
 
-var vals = [];
+    pattern_scale: 800,
+    pattern_octaves: 5,
+    pattern_lacunarity: 2,
+    pattern_gain: 0.7,
+  };
+}
 
 function render() {
   loadPixels();
@@ -21,24 +31,35 @@ function render() {
 
   for (var y = startY; y < endY; y++) {
     for (var x = 0; x < width; x++) {
-      var values = pattern(x, y, 800, 5, 2, 0.8);
+      var values = pattern(
+        x,
+        y,
+        root.pattern_scale,
+        root.pattern_octaves,
+        root.pattern_lacunarity,
+        root.pattern_gain
+      );
       var vecO = createVector(values.o[0], values.o[1]);
       var vecN = createVector(values.n[0], values.n[1]);
       var mappedV = map(values.v, 0.4, 2.1, 0, 1);
       var dotN = vecN.dot(vecN);
-      var mapDotN = map(dotN, 0, 9000, 0, 1);
+      var mapDotN = map(dotN, 4000, 11000, 0, 1);
       var v = values.v * 128;
 
-      var col1 = lerpColor(cPurple, cWineRed, mappedV * 0.5);
-      var col2 = lerpColor(col1, cWhiteGrey, mapDotN);
+      var col1 = lerpColor(root.cBlack, root.cBlue, mappedV * 0.8);
+      var col2 = lerpColor(col1, root.cBrown, mapDotN);
+      var col2_1 = lerpColor(col2, root.cWhiteGrey, mapDotN * 0.5);
       var col3 = lerpColor(
-        col2,
-        cLighterWineRed,
-        (((0.1 * vecO.y) / 40) * vecO.y) / 40
+        col2_1,
+        root.cClouds,
+        sin(vecO.x / 40) *
+          cos(vecO.y / 40) *
+          noise(vecN.x / 80, vecN.y / 80) *
+          0
       );
       var col4 = lerpColor(
         col3,
-        cBlue,
+        root.cBlack,
         0.1 * smoothstep(1.2, 1.3, abs(vecN.y) / 40 + abs(vecN.x) / 40)
       );
 
