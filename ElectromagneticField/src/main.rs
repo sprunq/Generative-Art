b3db97c6e2dd7b999cd3d895e1efae7fc0f0dba8
sprunq <tobias.laurin@web.de>
2022-07-11 16:58:20 +0200

    .

--- ElectromagneticField/src/main.rs
+++ ElectromagneticField/src/main.rs
@@ -1,18 +1,18 @@
+pub mod charge;
+pub mod particle;
 use charge::Charge;
-use nannou::{draw::mesh::vertex::Color, prelude::*};
+use nannou::{
+    draw::mesh::vertex::Color,
+    prelude::*,
+    rand::{prelude::StdRng, Rng},
+};
 use particle::Particle;
+use rand_core::SeedableRng;
 use rayon::{
     iter::{IntoParallelRefMutIterator, ParallelIterator},
     slice::ParallelSlice,
 };
-use std::{
-    ops::{Add, Mul},
-    thread,
-    time::Duration,
-};
-
-pub mod charge;
-pub mod particle;
+use std::ops::{Add, Mul};
 
 fn main() {
     nannou::app(model).update(update).run();
@@ -20,29 +20,46 @@ fn main() {
 
 struct Model {
     _window: window::Id,
+    random_seed: u64,
     charges: Vec<Charge>,
     col: usize,
     row: usize,
     sz: usize,
     points: Vec<Particle>,
     render_vectors: bool,
-}
 
-pub fn random_point_in_circle(max_r: f32, w: f32, h: f32) -> Vec2 {
-    let a = random_f32() * 2.0 * PI;
-    let r = 20.0 * (random_f32() * max_r).sqrt();
-    let x = w / 2.0 + r * a.cos();
-    let y = h / 2.0 + r * a.sin();
-    Vec2::new(x - w / 2.0, y - h / 2.0)
+    charge_count: usize,
+    charge_spawn_diameter: f32,
+    charge_range: std::ops::Range<f32>,
+
+    particle_count: usize,
+    particle_spawn_diameter: f32,
+    particle_speed: Vec2,
 }
 
 fn model(app: &App) -> Model {
     let _window = app
         .new_window()
-        .size(1000, 1000)
+        .size(1900, 1900)
         .view(view)
         .build()
         .unwrap();
+
+    let charge_count = 60;
+    let charge_spawn_diameter = 1500.0;
+    let charge_range = -0500000000.0..10000000000.0;
+
+    let particle_count = 70_000;
+    let particle_spawn_diameter = 3000.0;
+    let particle_speed = Vec2::new(5.0, 5.0);
+
+    let set_seed = 0;
+    let random_seed = if set_seed == 0 {
+        random_range(1, 200000000000000)
+    } else {
+        set_seed
+    };
+    let mut random = SeedableRng::seed_from_u64(random_seed);
 
     let screen_w = app.main_window().rect().w() as f32 * 1.0;
     let screen_h = app.main_window().rect().h() as f32 * 1.0;
@@ -52,20 +69,20 @@ fn model(app: &App) -> Model {
     let row = (screen_h / sz as f32) as usize;
 
     let mut charges = vec![];
-    for _ in 0..10 {
+    for _ in 0..charge_count {
         let charge = Charge::new(
-            random_point_in_circle(800.0, screen_w, screen_h),
-            random_range(-100_000.0, 100_000.0),
+            random_point_in_circle(charge_spawn_diameter, screen_w, screen_h, &mut random),
+            random.gen_range(charge_range.clone()),
         );
         charges.push(charge);
     }
 
     let mut points = Vec::<Particle>::new();
-    for _ in 0..50_000 {
+    for _ in 0..particle_count {
         let p = Particle::new(
-            random_point_in_circle(1000.0, screen_w, screen_h),
+            random_point_in_circle(particle_spawn_diameter, screen_w, screen_h, &mut random),
             Vec2::new(0.0, 0.0),
-            Vec2::new(10.0, 10.0),
+            particle_speed,
         );
         points.push(p);
     }
@@ -77,6 +94,13 @@ fn model(app: &App) -> Model {
         sz,
         points,
         render_vectors: false,
+        charge_count,
+        charge_spawn_diameter,
+        charge_range,
+        particle_count,
+        particle_spawn_diameter,
+        particle_speed,
+        random_seed,
     }
 }
 
@@ -120,20 +144,11 @@ fn view(app: &App, _model: &Model, frame: Frame) {
         }
     }
 
-    if app.keys.down.contains(&Key::S) {
-        print!("Printing");
-        let file_path = captured_frame_path(app);
-        app.main_window().capture_frame(file_path);
-        print!("Print done");
-
-        thread::sleep(Duration::from_millis(500));
-    }
-
     if app.elapsed_frames() != 200 {
         return;
     } else {
         print!("Printing");
-        let file_path = captured_frame_path(app);
+        let file_path = captured_frame_path(app, _model);
         app.main_window().capture_frame(file_path);
         print!("Print done");
     }
@@ -144,7 +159,7 @@ fn view(app: &App, _model: &Model, frame: Frame) {
     for particle in _model.points.clone() {
         let mut colored_points = Vec::new();
         for p in particle.trail_list.clone() {
-            let color = Color::new(0.0, 0.0, 0.0, 0.17);
+            let color = Color::new(0.0, 0.0, 0.0, 0.2);
             colored_points.push((p, color));
         }
         draw.polyline().points_colored(colored_points);
@@ -184,10 +199,40 @@ fn render_vectors(draw: &Draw, app: &App, _model: &Model) {
     }
 }
 
-fn captured_frame_path(app: &App) -> std::path::PathBuf {
+pub fn random_point_in_circle(max_r: f32, w: f32, h: f32, random: &mut StdRng) -> Vec2 {
+    let a = random.gen_range(0.0..1.0) * 2.0 * PI;
+    let r = 20.0 * (random.gen_range(0.0..1.0) * max_r).sqrt();
+    let x = w / 2.0 + r * a.cos();
+    let y = h / 2.0 + r * a.sin();
+    Vec2::new(x - w / 2.0, y - h / 2.0)
+}
+
+fn captured_frame_path(app: &App, model: &Model) -> std::path::PathBuf {
+    let path = [
+        "px",
+        &app.window_rect().w().to_string(),
+        &app.window_rect().h().to_string(),
+        "pc",
+        &model.particle_count.to_string(),
+        "psd",
+        &model.particle_spawn_diameter.to_string(),
+        "ps",
+        &model.particle_speed.to_string(),
+        "cc",
+        &model.charge_count.to_string(),
+        "csd",
+        &model.charge_spawn_diameter.to_string(),
+        "crs",
+        &model.charge_range.start.to_string(),
+        "to",
+        &model.charge_range.end.to_string(),
+        "seed",
+        &model.random_seed.to_string(),
+    ]
+    .join("_");
     app.project_path()
         .expect("failed to locate `project_path`")
         .join(app.exe_name().unwrap())
-        .join(format!("{}", (random_f32() * 1000000.0).to_string()))
+        .join(path)
         .with_extension("png")
 }
